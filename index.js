cde403d4350a9fed346b825436be5bae400f1720
harsha01raj <harshraj810k@gmail.com>
2024-08-16 21:54:41 +0530

    commit

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const auth = require("./middleware/authenticate.middleware.js");
 const bookRouter = require("./routes/book.route.js");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use("/user",UserRouter);
-app.use("/book",auth,bookRouter);
+app.use(cors({ origin: "*" }));
+app.use("/user", UserRouter);
+app.use("/book", auth, bookRouter);
 
-app.get("/",(req,res)=>{
-  res.status(200).json({"Message":"Your Server Works Fine.."})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ Message: "Your Server Works Fine.." });
+});
 
 app.listen(PORT, async () => {
   try {
